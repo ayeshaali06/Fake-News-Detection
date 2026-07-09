59a17d49a93a85a2f96033fa78d7e4f620c0d741
ayeshaali06 <ayeshaali4095@gmail.com>
2026-07-09 22:15:03 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,38 @@
-# Fake-News-Detection
+# Fake News Detection using Deep Learning (LSTM)
+
+## Project Overview
+This project detects whether a news article is Fake or Real using a Long Short-Term Memory (LSTM) deep learning model.
+
+## Features
+- Data preprocessing
+- Text cleaning
+- Tokenization
+- LSTM model
+- Fake/Real prediction
+- Streamlit web application
+
+## Technologies Used
+- Python
+- TensorFlow
+- Keras
+- Pandas
+- NumPy
+- Scikit-learn
+- NLTK
+- Streamlit
+
+## Model Accuracy
+Test Accuracy: ~99%
+
+## Dataset
+Fake and Real News Dataset
+
+## How to Run
+
+Install dependencies:
+
+pip install -r requirements.txt
+
+Run:
+
+streamlit run app.py
---
+++ app.py
@@ -0,0 +1,39 @@
+
+import streamlit as st
+import pickle
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+import re
+from nltk.corpus import stopwords
+
+# Load model and tokenizer
+model = load_model("fake_news_model.keras")
+
+with open("tokenizer.pkl", "rb") as f:
+    tokenizer = pickle.load(f)
+
+stop_words = set(stopwords.words("english"))
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r"http\S+", "", text)
+    text = re.sub(r"[^a-zA-Z]", " ", text)
+    words = text.split()
+    words = [word for word in words if word not in stop_words]
+    return " ".join(words)
+
+st.title("📰 Fake News Detection System")
+
+news = st.text_area("Enter News Article")
+
+if st.button("Predict"):
+    news = clean_text(news)
+    seq = tokenizer.texts_to_sequences([news])
+    padded = pad_sequences(seq, maxlen=300)
+
+    prediction = model.predict(padded)[0][0]
+
+    if prediction > 0.5:
+        st.success(f"✅ Real News ({prediction*100:.2f}%)")
+    else:
+        st.error(f"❌ Fake News ({(1-prediction)*100:.2f}%)")
